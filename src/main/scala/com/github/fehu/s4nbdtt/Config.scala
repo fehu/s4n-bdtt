7dd9706bc43eccf2c01e8f4f6f7898e0c1f2d1ec
Dmitry Kovalev <kdn.kovalev@gmail.com>
2020-10-28 01:23:34 -0600

    App configuration: ensure commands are not ambiguous.

--- src/main/scala/com/github/fehu/s4nbdtt/Config.scala
+++ src/main/scala/com/github/fehu/s4nbdtt/Config.scala
@@ -7,9 +7,10 @@ import cats.syntax.either._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.Positive
 import eu.timepit.refined.pureconfig._
-import pureconfig.ConfigSource
-import pureconfig.error.ConfigReaderException
+import pureconfig.{ ConfigReader, ConfigSource }
+import pureconfig.error.{ ConfigReaderException, FailureReason }
 import pureconfig.generic.auto._
+import pureconfig.generic.semiauto.deriveReader
 
 final case class Config(
   grid: Config.SymmetricGrid,
@@ -39,6 +40,20 @@ object Config {
 
   object Drone {
     final case class Commands(moveForward: Char, rotateLeft: Char, rotateRight: Char)
+
+    object Commands {
+      implicit def commandsConfigReader: ConfigReader[Commands] =
+        deriveReader[Commands].emap {
+          case Commands(mf, tl, _) if mf == tl => AmbiguousCommands(mf, "move forward", "rotate left").asLeft
+          case Commands(mf, _, tr) if mf == tr => AmbiguousCommands(mf, "move forward", "rotate right").asLeft
+          case Commands(_, tl, tr) if tl == tr => AmbiguousCommands(tl, "rotate left", "rotate right").asLeft
+          case cmds                            => cmds.asRight
+        }
+
+      final case class AmbiguousCommands(ambiguous: Char, cmd1: String, cmd2: String) extends FailureReason {
+        def description: String = s"""Same character '$ambiguous' is defined for commands "$cmd1" and "$cmd2"."""
+      }
+    }
   }
 
   final case class Files(path: Path, prefix: String, suffix: String)
